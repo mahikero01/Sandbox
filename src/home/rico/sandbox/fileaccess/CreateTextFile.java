d0f7c69f08120d1616a69ae9dcdf98327d790e7d
Rico - Laptop <federicoparassarmiento@yahoo.com>
2014-12-07 19:57:14 +0800

    up

--- src/home/rico/sandbox/fileaccess/CreateTextFile.java
+++ src/home/rico/sandbox/fileaccess/CreateTextFile.java
@@ -1,5 +1,32 @@
 package home.rico.sandbox.fileaccess;
 
-public class CreateTextFile {
+import java.io.FileNotFoundException;
+import java.lang.SecurityException;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.util.NoSuchElementException;
+
+public class CreateTextFile 
+{
+	
+	private static Formatter output;
+	
+	public static void openFile()
+	{
+		try
+		{
+			output = new Formatter("clients.txt");
+		}
+		catch (SecurityException securityException)
+		{
+			System.err.println("Write permission denied");
+			System.exit(1);
+		}
+		catch (FileNotFoundException fileNotFoundException)
+		{
+			System.err.println("Error opening file");
+			System.exit(1);
+		}
+	}
 
 }
