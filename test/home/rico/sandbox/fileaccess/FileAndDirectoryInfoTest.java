bbb60dd8dfc1c9754466b61d2e2938e6bbee0330
Rico - Laptop <federicoparassarmiento@yahoo.com>
2014-12-07 16:01:56 +0800

    Test Files

---
+++ test/home/rico/sandbox/fileaccess/FileAndDirectoryInfoTest.java
@@ -0,0 +1,21 @@
+package home.rico.sandbox.fileaccess;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class FileAndDirectoryInfoTest {
+
+	@Test
+	public void test() throws IOException {
+		FileAndDirectoryInfo info1 = new FileAndDirectoryInfo();
+		
+		info1.checkPath();
+		
+		
+		
+	}
+
+}
